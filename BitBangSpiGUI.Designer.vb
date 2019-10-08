02d342224e561355ce791bf73560aab4e0d225d7
anolan3 <Alex.Nolan@analog.com>
2019-10-08 12:48:02 -0400

    Added basic bit bang SPI GUI implementation

--- BitBangSpiGUI.Designer.vb
+++ BitBangSpiGUI.Designer.vb
@@ -21,8 +21,133 @@ Partial Class BitBangSpiGUI
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.btn_Transfer = New System.Windows.Forms.Button()
+        Me.MISOData = New System.Windows.Forms.TextBox()
+        Me.bitsPerTransfer = New System.Windows.Forms.TextBox()
+        Me.MOSIData = New System.Windows.Forms.TextBox()
+        Me.numTransfers = New System.Windows.Forms.TextBox()
+        Me.btn_restoreSpi = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(12, 9)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(88, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Bits Per Transfer:"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(13, 61)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(89, 13)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "MOSI Data (hex):"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(12, 35)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(64, 13)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "# Transfers:"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(13, 167)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(89, 13)
+        Me.Label4.TabIndex = 3
+        Me.Label4.Text = "MISO Data (hex):"
+        '
+        'btn_Transfer
+        '
+        Me.btn_Transfer.Location = New System.Drawing.Point(12, 98)
+        Me.btn_Transfer.Name = "btn_Transfer"
+        Me.btn_Transfer.Size = New System.Drawing.Size(90, 42)
+        Me.btn_Transfer.TabIndex = 4
+        Me.btn_Transfer.Text = "Transfer Data"
+        Me.btn_Transfer.UseVisualStyleBackColor = True
+        '
+        'MISOData
+        '
+        Me.MISOData.Location = New System.Drawing.Point(106, 164)
+        Me.MISOData.Name = "MISOData"
+        Me.MISOData.Size = New System.Drawing.Size(167, 20)
+        Me.MISOData.TabIndex = 5
+        '
+        'bitsPerTransfer
+        '
+        Me.bitsPerTransfer.Location = New System.Drawing.Point(106, 6)
+        Me.bitsPerTransfer.Name = "bitsPerTransfer"
+        Me.bitsPerTransfer.Size = New System.Drawing.Size(167, 20)
+        Me.bitsPerTransfer.TabIndex = 6
+        Me.bitsPerTransfer.Text = "16"
+        '
+        'MOSIData
+        '
+        Me.MOSIData.Location = New System.Drawing.Point(106, 58)
+        Me.MOSIData.Name = "MOSIData"
+        Me.MOSIData.Size = New System.Drawing.Size(167, 20)
+        Me.MOSIData.TabIndex = 7
+        Me.MOSIData.Text = "0000"
+        '
+        'numTransfers
+        '
+        Me.numTransfers.Location = New System.Drawing.Point(106, 32)
+        Me.numTransfers.Name = "numTransfers"
+        Me.numTransfers.Size = New System.Drawing.Size(167, 20)
+        Me.numTransfers.TabIndex = 8
+        Me.numTransfers.Text = "1"
+        '
+        'btn_restoreSpi
+        '
+        Me.btn_restoreSpi.Location = New System.Drawing.Point(183, 98)
+        Me.btn_restoreSpi.Name = "btn_restoreSpi"
+        Me.btn_restoreSpi.Size = New System.Drawing.Size(90, 42)
+        Me.btn_restoreSpi.TabIndex = 9
+        Me.btn_restoreSpi.Text = "Restore Hardware SPI"
+        Me.btn_restoreSpi.UseVisualStyleBackColor = True
+        '
+        'BitBangSpiGUI
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(285, 189)
+        Me.Controls.Add(Me.btn_restoreSpi)
+        Me.Controls.Add(Me.numTransfers)
+        Me.Controls.Add(Me.MOSIData)
+        Me.Controls.Add(Me.bitsPerTransfer)
+        Me.Controls.Add(Me.MISOData)
+        Me.Controls.Add(Me.btn_Transfer)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "BitBangSpiGUI"
         Me.Text = "BitBangSpiGUI"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
     End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents btn_Transfer As Button
+    Friend WithEvents MISOData As TextBox
+    Friend WithEvents bitsPerTransfer As TextBox
+    Friend WithEvents MOSIData As TextBox
+    Friend WithEvents numTransfers As TextBox
+    Friend WithEvents btn_restoreSpi As Button
 End Class
--- BitBangSpiGUI.vb
+++ BitBangSpiGUI.vb
@@ -1,7 +1,45 @@
-﻿Public Class BitBangSpiGUI
+﻿Imports FX3Api
+
+Public Class BitBangSpiGUI
     Inherits FormBase
 
     Private Sub BitBangSpiGUI_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        m_TopGUI.FX3.BitBangSpiConfig = New BitBangSpiConfig(True)
+        m_TopGUI.FX3.SetBitBangSpiFreq(500000)
+    End Sub
 
+    Private Sub Shutdown() Handles Me.Closing
+        'restore hardware SPI
+        m_TopGUI.FX3.RestoreHardwareSpi()
+    End Sub
+
+    Private Sub btn_Transfer_Click(sender As Object, e As EventArgs) Handles btn_Transfer.Click
+
+        Dim transfers, bptransfer As UInteger
+        Dim MOSI As New List(Of Byte)
+        Dim MISO As Byte()
+        Dim MISOStr As String
+        Try
+            transfers = Convert.ToUInt32(numTransfers.Text)
+            bptransfer = Convert.ToUInt32(bitsPerTransfer.Text)
+            Dim byteStr As String
+            For i As Integer = 0 To MOSIData.Text.Length() - 1 Step 2
+                byteStr = MOSIData.Text.Substring(i, 2)
+                MOSI.Add(Convert.ToByte(byteStr, 16))
+            Next
+            MISO = m_TopGUI.FX3.BitBangSpi(bptransfer, transfers, MOSI.ToArray(), 5000)
+            MISOStr = ""
+            For Each value In MISO
+                MISOStr = MISOStr + value.ToString("X2")
+            Next
+            MISOData.Text = MISOStr
+        Catch ex As Exception
+            MsgBox("ERROR: Invalid settings. " + ex.Message())
+        End Try
+
+    End Sub
+
+    Private Sub btn_restoreSpi_Click(sender As Object, e As EventArgs) Handles btn_restoreSpi.Click
+        m_TopGUI.FX3.RestoreHardwareSpi()
     End Sub
 End Class
