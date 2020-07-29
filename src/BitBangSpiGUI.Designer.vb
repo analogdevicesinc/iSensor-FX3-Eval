cafbf5772a55763bd7c8cfbc6c9f31f56f497745
anolan3 <Alex.Nolan@analog.com>
2020-07-29 11:40:49 -0400

    Reworked bit bang SPI gui to use data grid view

--- src/BitBangSpiGUI.Designer.vb
+++ src/BitBangSpiGUI.Designer.vb
@@ -22,13 +22,9 @@ Partial Class BitBangSpiGUI
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
         Me.Label1 = New System.Windows.Forms.Label()
-        Me.Label2 = New System.Windows.Forms.Label()
         Me.Label3 = New System.Windows.Forms.Label()
-        Me.Label4 = New System.Windows.Forms.Label()
         Me.btn_Transfer = New System.Windows.Forms.Button()
-        Me.MISOData = New System.Windows.Forms.TextBox()
         Me.bitsPerTransfer = New System.Windows.Forms.TextBox()
-        Me.MOSIData = New System.Windows.Forms.TextBox()
         Me.numTransfers = New System.Windows.Forms.TextBox()
         Me.btn_restoreSpi = New System.Windows.Forms.Button()
         Me.sclk_freq = New System.Windows.Forms.TextBox()
@@ -39,6 +35,8 @@ Partial Class BitBangSpiGUI
         Me.Label7 = New System.Windows.Forms.Label()
         Me.csLag = New System.Windows.Forms.TextBox()
         Me.Label8 = New System.Windows.Forms.Label()
+        Me.result = New System.Windows.Forms.DataGridView()
+        CType(Me.result, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
         'Label1
@@ -50,15 +48,6 @@ Partial Class BitBangSpiGUI
         Me.Label1.TabIndex = 0
         Me.Label1.Text = "Bits Per Transfer:"
         '
-        'Label2
-        '
-        Me.Label2.AutoSize = True
-        Me.Label2.Location = New System.Drawing.Point(12, 61)
-        Me.Label2.Name = "Label2"
-        Me.Label2.Size = New System.Drawing.Size(89, 13)
-        Me.Label2.TabIndex = 1
-        Me.Label2.Text = "MOSI Data (hex):"
-        '
         'Label3
         '
         Me.Label3.AutoSize = True
@@ -68,30 +57,14 @@ Partial Class BitBangSpiGUI
         Me.Label3.TabIndex = 2
         Me.Label3.Text = "# Transfers:"
         '
-        'Label4
-        '
-        Me.Label4.AutoSize = True
-        Me.Label4.Location = New System.Drawing.Point(9, 257)
-        Me.Label4.Name = "Label4"
-        Me.Label4.Size = New System.Drawing.Size(89, 13)
-        Me.Label4.TabIndex = 3
-        Me.Label4.Text = "MISO Data (hex):"
-        '
         'btn_Transfer
         '
-        Me.btn_Transfer.Location = New System.Drawing.Point(15, 195)
+        Me.btn_Transfer.Location = New System.Drawing.Point(15, 163)
         Me.btn_Transfer.Name = "btn_Transfer"
         Me.btn_Transfer.Size = New System.Drawing.Size(90, 42)
         Me.btn_Transfer.TabIndex = 4
         Me.btn_Transfer.Text = "Transfer Data"
         Me.btn_Transfer.UseVisualStyleBackColor = True
-        '
-        'MISOData
-        '
-        Me.MISOData.Location = New System.Drawing.Point(105, 254)
-        Me.MISOData.Name = "MISOData"
-        Me.MISOData.Size = New System.Drawing.Size(167, 20)
-        Me.MISOData.TabIndex = 5
         '
         'bitsPerTransfer
         '
@@ -100,14 +73,6 @@ Partial Class BitBangSpiGUI
         Me.bitsPerTransfer.Size = New System.Drawing.Size(167, 20)
         Me.bitsPerTransfer.TabIndex = 6
         Me.bitsPerTransfer.Text = "16"
-        '
-        'MOSIData
-        '
-        Me.MOSIData.Location = New System.Drawing.Point(105, 58)
-        Me.MOSIData.Name = "MOSIData"
-        Me.MOSIData.Size = New System.Drawing.Size(167, 20)
-        Me.MOSIData.TabIndex = 7
-        Me.MOSIData.Text = "0000"
         '
         'numTransfers
         '
@@ -119,7 +84,7 @@ Partial Class BitBangSpiGUI
         '
         'btn_restoreSpi
         '
-        Me.btn_restoreSpi.Location = New System.Drawing.Point(182, 195)
+        Me.btn_restoreSpi.Location = New System.Drawing.Point(182, 163)
         Me.btn_restoreSpi.Name = "btn_restoreSpi"
         Me.btn_restoreSpi.Size = New System.Drawing.Size(90, 42)
         Me.btn_restoreSpi.TabIndex = 9
@@ -128,7 +93,7 @@ Partial Class BitBangSpiGUI
         '
         'sclk_freq
         '
-        Me.sclk_freq.Location = New System.Drawing.Point(105, 84)
+        Me.sclk_freq.Location = New System.Drawing.Point(105, 59)
         Me.sclk_freq.Name = "sclk_freq"
         Me.sclk_freq.Size = New System.Drawing.Size(167, 20)
         Me.sclk_freq.TabIndex = 11
@@ -137,7 +102,7 @@ Partial Class BitBangSpiGUI
         'Label5
         '
         Me.Label5.AutoSize = True
-        Me.Label5.Location = New System.Drawing.Point(12, 87)
+        Me.Label5.Location = New System.Drawing.Point(12, 62)
         Me.Label5.Name = "Label5"
         Me.Label5.Size = New System.Drawing.Size(83, 13)
         Me.Label5.TabIndex = 10
@@ -145,7 +110,7 @@ Partial Class BitBangSpiGUI
         '
         'csLead
         '
-        Me.csLead.Location = New System.Drawing.Point(105, 136)
+        Me.csLead.Location = New System.Drawing.Point(105, 111)
         Me.csLead.Name = "csLead"
         Me.csLead.Size = New System.Drawing.Size(167, 20)
         Me.csLead.TabIndex = 13
@@ -154,7 +119,7 @@ Partial Class BitBangSpiGUI
         'Label6
         '
         Me.Label6.AutoSize = True
-        Me.Label6.Location = New System.Drawing.Point(12, 139)
+        Me.Label6.Location = New System.Drawing.Point(12, 114)
         Me.Label6.Name = "Label6"
         Me.Label6.Size = New System.Drawing.Size(80, 13)
         Me.Label6.TabIndex = 12
@@ -162,7 +127,7 @@ Partial Class BitBangSpiGUI
         '
         'stallTicks
         '
-        Me.stallTicks.Location = New System.Drawing.Point(105, 110)
+        Me.stallTicks.Location = New System.Drawing.Point(105, 85)
         Me.stallTicks.Name = "stallTicks"
         Me.stallTicks.Size = New System.Drawing.Size(167, 20)
         Me.stallTicks.TabIndex = 15
@@ -171,7 +136,7 @@ Partial Class BitBangSpiGUI
         'Label7
         '
         Me.Label7.AutoSize = True
-        Me.Label7.Location = New System.Drawing.Point(12, 113)
+        Me.Label7.Location = New System.Drawing.Point(12, 88)
         Me.Label7.Name = "Label7"
         Me.Label7.Size = New System.Drawing.Size(76, 13)
         Me.Label7.TabIndex = 14
@@ -179,7 +144,7 @@ Partial Class BitBangSpiGUI
         '
         'csLag
         '
-        Me.csLag.Location = New System.Drawing.Point(105, 162)
+        Me.csLag.Location = New System.Drawing.Point(105, 137)
         Me.csLag.Name = "csLag"
         Me.csLag.Size = New System.Drawing.Size(167, 20)
         Me.csLag.TabIndex = 17
@@ -188,17 +153,31 @@ Partial Class BitBangSpiGUI
         'Label8
         '
         Me.Label8.AutoSize = True
-        Me.Label8.Location = New System.Drawing.Point(12, 165)
+        Me.Label8.Location = New System.Drawing.Point(12, 140)
         Me.Label8.Name = "Label8"
         Me.Label8.Size = New System.Drawing.Size(74, 13)
         Me.Label8.TabIndex = 16
         Me.Label8.Text = "CS Lag Ticks:"
         '
+        'result
+        '
+        Me.result.AllowUserToAddRows = False
+        Me.result.AllowUserToDeleteRows = False
+        Me.result.AllowUserToResizeColumns = False
+        Me.result.AllowUserToResizeRows = False
+        Me.result.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.result.Location = New System.Drawing.Point(15, 211)
+        Me.result.Name = "result"
+        Me.result.RowHeadersVisible = False
+        Me.result.Size = New System.Drawing.Size(257, 238)
+        Me.result.TabIndex = 18
+        '
         'BitBangSpiGUI
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(284, 289)
+        Me.ClientSize = New System.Drawing.Size(284, 461)
+        Me.Controls.Add(Me.result)
         Me.Controls.Add(Me.csLag)
         Me.Controls.Add(Me.Label8)
         Me.Controls.Add(Me.stallTicks)
@@ -209,29 +188,25 @@ Partial Class BitBangSpiGUI
         Me.Controls.Add(Me.Label5)
         Me.Controls.Add(Me.btn_restoreSpi)
         Me.Controls.Add(Me.numTransfers)
-        Me.Controls.Add(Me.MOSIData)
         Me.Controls.Add(Me.bitsPerTransfer)
-        Me.Controls.Add(Me.MISOData)
         Me.Controls.Add(Me.btn_Transfer)
-        Me.Controls.Add(Me.Label4)
         Me.Controls.Add(Me.Label3)
-        Me.Controls.Add(Me.Label2)
         Me.Controls.Add(Me.Label1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Sizable
+        Me.MaximumSize = New System.Drawing.Size(300, 1200)
+        Me.MinimumSize = New System.Drawing.Size(300, 300)
         Me.Name = "BitBangSpiGUI"
         Me.Text = "Bit Bang SPI"
+        CType(Me.result, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
     End Sub
 
     Friend WithEvents Label1 As Label
-    Friend WithEvents Label2 As Label
     Friend WithEvents Label3 As Label
-    Friend WithEvents Label4 As Label
     Friend WithEvents btn_Transfer As Button
-    Friend WithEvents MISOData As TextBox
     Friend WithEvents bitsPerTransfer As TextBox
-    Friend WithEvents MOSIData As TextBox
     Friend WithEvents numTransfers As TextBox
     Friend WithEvents btn_restoreSpi As Button
     Friend WithEvents sclk_freq As TextBox
@@ -242,4 +217,5 @@ Partial Class BitBangSpiGUI
     Friend WithEvents Label7 As Label
     Friend WithEvents csLag As TextBox
     Friend WithEvents Label8 As Label
+    Friend WithEvents result As DataGridView
 End Class
--- src/BitBangSpiGUI.vb
+++ src/BitBangSpiGUI.vb
@@ -8,11 +8,27 @@ Imports FX3Api
 Public Class BitBangSpiGUI
     Inherits FormBase
 
-    Private Sub BitBangSpiGUI_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+    Private numBytes As Integer
+    Private transfers As UInteger
+    Private bptransfer As UInteger
+    Private goodParams As Boolean
+
+    Private Sub BitBangSpiGUI_Load(sender As Object, e As EventArgs) Handles Me.Load
         m_TopGUI.FX3.BitBangSpiConfig = New BitBangSpiConfig(True)
         csLag.Text = m_TopGUI.FX3.BitBangSpiConfig.CSLagTicks.ToString()
         csLead.Text = m_TopGUI.FX3.BitBangSpiConfig.CSLeadTicks.ToString()
         stallTicks.Text = 10.0
+
+        result.ColumnCount = 3
+        result.Columns(0).Name = ("Byte Number")
+        result.Columns(1).Name = ("MISO Value")
+        result.Columns(2).Name = ("MOSI Value")
+        result.Columns(0).AutoSizeMode = DataGridViewAutoSizeColumnMode.DisplayedCells
+        result.Columns(1).AutoSizeMode = DataGridViewAutoSizeColumnMode.DisplayedCells
+        result.Columns(2).AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill
+        UpdateParameters()
+        SetupResultView()
+
     End Sub
 
     Private Sub Shutdown() Handles Me.Closing
@@ -22,31 +38,21 @@ Public Class BitBangSpiGUI
     End Sub
 
     Private Sub btn_Transfer_Click(sender As Object, e As EventArgs) Handles btn_Transfer.Click
-
-        Dim transfers, bptransfer As UInteger
         Dim MOSI As New List(Of Byte)
         Dim MISO As Byte()
-        Dim MISOStr As String
         Try
-            transfers = Convert.ToUInt32(numTransfers.Text)
-            bptransfer = Convert.ToUInt32(bitsPerTransfer.Text)
-            m_TopGUI.FX3.SetBitBangSpiFreq(Convert.ToDouble(sclk_freq.Text))
-            m_TopGUI.FX3.SetBitBangStallTime(Convert.ToDouble(stallTicks.Text))
-            m_TopGUI.FX3.BitBangSpiConfig.CSLagTicks = Convert.ToUInt16(csLag.Text)
-            m_TopGUI.FX3.BitBangSpiConfig.CSLeadTicks = Convert.ToUInt16(csLead.Text)
-            Dim byteStr As String
-            For i As Integer = 0 To MOSIData.Text.Length() - 1 Step 2
-                byteStr = MOSIData.Text.Substring(i, 2)
-                MOSI.Add(Convert.ToByte(byteStr, 16))
+            'parse input data
+            For i As Integer = 0 To numBytes - 1
+                MOSI.Add(Convert.ToUInt32(result.Item("MOSI Value", i).Value, 16))
             Next
+            'transfer SPI data
             MISO = m_TopGUI.FX3.BitBangSpi(bptransfer, transfers, MOSI.ToArray(), 5000)
-            MISOStr = ""
-            For Each value In MISO
-                MISOStr = MISOStr + value.ToString("X2")
+            'update output
+            For i As Integer = 0 To numBytes - 1
+                result.Item("MISO Value", i).Value = "0x" + MISO(i).ToString("X2")
             Next
-            MISOData.Text = MISOStr
         Catch ex As Exception
-            MsgBox("ERROR: Invalid settings. " + ex.Message())
+            MsgBox("ERROR: " + ex.Message())
         End Try
 
     End Sub
@@ -58,4 +64,46 @@ Public Class BitBangSpiGUI
     Private Sub RestoreSPI() Handles Me.Deactivate
         m_TopGUI.FX3.RestoreHardwareSpi()
     End Sub
+
+    Private Sub ParamsChanged() Handles bitsPerTransfer.TextChanged, numTransfers.TextChanged
+        UpdateParameters()
+        SetupResultView()
+    End Sub
+
+    Private Sub UpdateParameters()
+        goodParams = True
+        Try
+            transfers = Convert.ToUInt32(numTransfers.Text)
+            bptransfer = Convert.ToUInt32(bitsPerTransfer.Text)
+            numBytes = CUInt(Math.Ceiling(bptransfer / 8)) * transfers
+            m_TopGUI.FX3.SetBitBangSpiFreq(Convert.ToDouble(sclk_freq.Text))
+            m_TopGUI.FX3.SetBitBangStallTime(Convert.ToDouble(stallTicks.Text))
+            m_TopGUI.FX3.BitBangSpiConfig.CSLagTicks = Convert.ToUInt16(csLag.Text)
+            m_TopGUI.FX3.BitBangSpiConfig.CSLeadTicks = Convert.ToUInt16(csLead.Text)
+        Catch ex As Exception
+            'squash
+            goodParams = False
+        End Try
+    End Sub
+
+    Private Sub SetupResultView()
+        If result.ColumnCount <> 3 Then Return
+        If Not goodParams Then Return
+        If result.RowCount < numBytes Then
+            For i As Integer = result.RowCount To numBytes - 1
+                result.Rows.Add({i.ToString(), "", "0x00"})
+            Next
+        ElseIf result.RowCount > numBytes Then
+            Dim numRows = result.RowCount - numBytes
+            For i As Integer = 0 To numRows - 1
+                result.Rows.RemoveAt(result.RowCount - 1)
+            Next
+        End If
+
+    End Sub
+
+    Private Sub ResizeHandler() Handles Me.SizeChanged
+        result.Height = Me.Height - 262
+    End Sub
+
 End Class
