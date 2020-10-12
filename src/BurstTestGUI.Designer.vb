8938e1e074ef493ab7a2ed860e5232560c6ede27
anolan3 <Alex.Nolan@analog.com>
2020-10-12 00:02:26 -0400

    Added generic burst logging stream to burst test GUI

--- src/BurstTestGUI.Designer.vb
+++ src/BurstTestGUI.Designer.vb
@@ -31,13 +31,18 @@ Partial Class BurstTestGUI
         Me.Label2 = New System.Windows.Forms.Label()
         Me.Label3 = New System.Windows.Forms.Label()
         Me.result = New System.Windows.Forms.DataGridView()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.btn_StartStream = New System.Windows.Forms.Button()
+        Me.numBurstWords = New System.Windows.Forms.TextBox()
+        Me.Label4 = New System.Windows.Forms.Label()
         CType(Me.result, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.GroupBox1.SuspendLayout()
         Me.SuspendLayout()
         '
         'drActive
         '
         Me.drActive.AutoSize = True
-        Me.drActive.Location = New System.Drawing.Point(72, 12)
+        Me.drActive.Location = New System.Drawing.Point(9, 108)
         Me.drActive.Name = "drActive"
         Me.drActive.Size = New System.Drawing.Size(75, 17)
         Me.drActive.TabIndex = 0
@@ -47,48 +52,48 @@ Partial Class BurstTestGUI
         'csDelay
         '
         Me.csDelay.FormattingEnabled = True
-        Me.csDelay.Location = New System.Drawing.Point(72, 35)
+        Me.csDelay.Location = New System.Drawing.Point(66, 23)
         Me.csDelay.Name = "csDelay"
-        Me.csDelay.Size = New System.Drawing.Size(175, 21)
+        Me.csDelay.Size = New System.Drawing.Size(195, 21)
         Me.csDelay.TabIndex = 1
         '
         'sclk
         '
-        Me.sclk.Location = New System.Drawing.Point(72, 62)
+        Me.sclk.Location = New System.Drawing.Point(66, 50)
         Me.sclk.Name = "sclk"
-        Me.sclk.Size = New System.Drawing.Size(175, 20)
+        Me.sclk.Size = New System.Drawing.Size(195, 20)
         Me.sclk.TabIndex = 2
         '
         'applySettings
         '
-        Me.applySettings.Location = New System.Drawing.Point(12, 114)
+        Me.applySettings.Location = New System.Drawing.Point(173, 102)
         Me.applySettings.Name = "applySettings"
-        Me.applySettings.Size = New System.Drawing.Size(75, 45)
+        Me.applySettings.Size = New System.Drawing.Size(88, 27)
         Me.applySettings.TabIndex = 3
         Me.applySettings.Text = "Apply Settings"
         Me.applySettings.UseVisualStyleBackColor = True
         '
         'captureData
         '
-        Me.captureData.Location = New System.Drawing.Point(172, 114)
+        Me.captureData.Location = New System.Drawing.Point(12, 150)
         Me.captureData.Name = "captureData"
         Me.captureData.Size = New System.Drawing.Size(75, 45)
         Me.captureData.TabIndex = 4
-        Me.captureData.Text = "Capture Data"
+        Me.captureData.Text = "Capture Burst Word"
         Me.captureData.UseVisualStyleBackColor = True
         '
         'numBytes
         '
-        Me.numBytes.Location = New System.Drawing.Point(72, 88)
+        Me.numBytes.Location = New System.Drawing.Point(66, 76)
         Me.numBytes.Name = "numBytes"
-        Me.numBytes.Size = New System.Drawing.Size(175, 20)
+        Me.numBytes.Size = New System.Drawing.Size(195, 20)
         Me.numBytes.TabIndex = 5
         Me.numBytes.Text = "48"
         '
         'Label1
         '
         Me.Label1.AutoSize = True
-        Me.Label1.Location = New System.Drawing.Point(12, 38)
+        Me.Label1.Location = New System.Drawing.Point(6, 26)
         Me.Label1.Name = "Label1"
         Me.Label1.Size = New System.Drawing.Size(54, 13)
         Me.Label1.TabIndex = 6
@@ -97,7 +102,7 @@ Partial Class BurstTestGUI
         'Label2
         '
         Me.Label2.AutoSize = True
-        Me.Label2.Location = New System.Drawing.Point(12, 65)
+        Me.Label2.Location = New System.Drawing.Point(6, 53)
         Me.Label2.Name = "Label2"
         Me.Label2.Size = New System.Drawing.Size(37, 13)
         Me.Label2.TabIndex = 7
@@ -106,7 +111,7 @@ Partial Class BurstTestGUI
         'Label3
         '
         Me.Label3.AutoSize = True
-        Me.Label3.Location = New System.Drawing.Point(12, 91)
+        Me.Label3.Location = New System.Drawing.Point(6, 79)
         Me.Label3.Name = "Label3"
         Me.Label3.Size = New System.Drawing.Size(46, 13)
         Me.Label3.TabIndex = 8
@@ -118,33 +123,74 @@ Partial Class BurstTestGUI
         Me.result.AllowUserToDeleteRows = False
         Me.result.AllowUserToResizeColumns = False
         Me.result.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
-        Me.result.Location = New System.Drawing.Point(12, 165)
+        Me.result.Location = New System.Drawing.Point(12, 201)
         Me.result.Name = "result"
         Me.result.RowHeadersVisible = False
-        Me.result.Size = New System.Drawing.Size(235, 293)
+        Me.result.Size = New System.Drawing.Size(267, 258)
         Me.result.TabIndex = 9
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.csDelay)
+        Me.GroupBox1.Controls.Add(Me.sclk)
+        Me.GroupBox1.Controls.Add(Me.Label3)
+        Me.GroupBox1.Controls.Add(Me.applySettings)
+        Me.GroupBox1.Controls.Add(Me.numBytes)
+        Me.GroupBox1.Controls.Add(Me.drActive)
+        Me.GroupBox1.Controls.Add(Me.Label2)
+        Me.GroupBox1.Controls.Add(Me.Label1)
+        Me.GroupBox1.Location = New System.Drawing.Point(12, 9)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(267, 135)
+        Me.GroupBox1.TabIndex = 10
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "SPI Settings"
+        '
+        'btn_StartStream
+        '
+        Me.btn_StartStream.Location = New System.Drawing.Point(93, 150)
+        Me.btn_StartStream.Name = "btn_StartStream"
+        Me.btn_StartStream.Size = New System.Drawing.Size(75, 45)
+        Me.btn_StartStream.TabIndex = 11
+        Me.btn_StartStream.Text = "Start Burst Stream"
+        Me.btn_StartStream.UseVisualStyleBackColor = True
+        '
+        'numBurstWords
+        '
+        Me.numBurstWords.Location = New System.Drawing.Point(174, 175)
+        Me.numBurstWords.Name = "numBurstWords"
+        Me.numBurstWords.Size = New System.Drawing.Size(105, 20)
+        Me.numBurstWords.TabIndex = 12
+        Me.numBurstWords.Text = "1000"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(174, 155)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(72, 13)
+        Me.Label4.TabIndex = 13
+        Me.Label4.Text = "#Burst Words"
         '
         'BurstTestGUI
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(259, 471)
+        Me.ClientSize = New System.Drawing.Size(292, 471)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.numBurstWords)
+        Me.Controls.Add(Me.btn_StartStream)
+        Me.Controls.Add(Me.GroupBox1)
         Me.Controls.Add(Me.result)
-        Me.Controls.Add(Me.Label3)
-        Me.Controls.Add(Me.Label2)
-        Me.Controls.Add(Me.Label1)
-        Me.Controls.Add(Me.numBytes)
         Me.Controls.Add(Me.captureData)
-        Me.Controls.Add(Me.applySettings)
-        Me.Controls.Add(Me.sclk)
-        Me.Controls.Add(Me.csDelay)
-        Me.Controls.Add(Me.drActive)
         Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Sizable
-        Me.MaximumSize = New System.Drawing.Size(275, 1500)
-        Me.MinimumSize = New System.Drawing.Size(275, 300)
+        Me.MaximumSize = New System.Drawing.Size(308, 1500)
+        Me.MinimumSize = New System.Drawing.Size(308, 300)
         Me.Name = "BurstTestGUI"
         Me.Text = "Burst Mode Test"
         CType(Me.result, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
@@ -160,4 +206,8 @@ Partial Class BurstTestGUI
     Friend WithEvents Label2 As Label
     Friend WithEvents Label3 As Label
     Friend WithEvents result As DataGridView
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents btn_StartStream As Button
+    Friend WithEvents numBurstWords As TextBox
+    Friend WithEvents Label4 As Label
 End Class
--- src/BurstTestGUI.vb
+++ src/BurstTestGUI.vb
@@ -5,11 +5,13 @@
 
 Imports FX3Api
 Imports RegMapClasses
+Imports StreamDataLogger
 
 Public Class BurstTestGUI
     Inherits FormBase
 
     Private m_numBytes As Integer
+    Private WithEvents fileManager As Logger
 
     Public Sub FormSetup() Handles Me.Load
         sclk.Text = m_TopGUI.FX3.SclkFrequency.ToString()
@@ -31,24 +33,13 @@ Public Class BurstTestGUI
     End Sub
 
     Public Sub ResizeHandler() Handles Me.Resize
-        result.Height = Me.Height - 217
+        result.Height = Me.Height - 252
     End Sub
 
     Private Sub captureData_Click(sender As Object, e As EventArgs) Handles captureData.Click
-        Dim burstTrigger As New List(Of Byte)
-        Dim byteVal As Byte
-        Try
-            For i As Integer = 0 To m_numBytes - 1
-                byteVal = Convert.ToUInt32(result.Item("MOSI Value", i).Value, 16)
-                burstTrigger.Add(byteVal)
-            Next
-        Catch ex As Exception
-            MsgBox("ERROR: Invalid MOSI data. " + ex.Message)
-        End Try
-        m_TopGUI.FX3.TriggerReg = New RegMapClasses.RegClass With {.Address = 0}
-        m_TopGUI.FX3.StripBurstTriggerWord = False
-        m_TopGUI.FX3.BurstByteCount = m_numBytes
-        m_TopGUI.FX3.SetupBurstMode()
+
+        Dim burstTrigger() As Byte = UpdateBurstParams()
+        If IsNothing(burstTrigger) Then Exit Sub
         m_TopGUI.FX3.StartBurstStream(1, burstTrigger)
         While m_TopGUI.FX3.GetNumBuffersRead < 1
             System.Threading.Thread.Sleep(10)
@@ -95,6 +86,12 @@ Public Class BurstTestGUI
     Private Sub Shutdown() Handles Me.Closing
         m_TopGUI.FX3.StripBurstTriggerWord = True
         m_TopGUI.btn_BurstTest.Enabled = True
+
+        'dispose
+        If Not IsNothing(fileManager) Then
+            fileManager.Dispose()
+        End If
+        Me.Dispose()
     End Sub
 
     Private Sub num32words_TextChanged(sender As Object, e As EventArgs) Handles numBytes.LostFocus
@@ -104,5 +101,120 @@ Public Class BurstTestGUI
         Catch ex As Exception
         End Try
     End Sub
+
+    Private Sub btn_StartStream_Click(sender As Object, e As EventArgs) Handles btn_StartStream.Click
+        Dim savePath As String
+        Dim numBursts As UInteger
+        Dim burstTrigger() As Byte
+        Dim regs As New List(Of RegClass)
+        Dim timeString As String
+
+        'check if stream start/stop needed
+        If btn_StartStream.Text = "Start Burst Stream" Then
+            'check dr (if selected)
+            If m_TopGUI.FX3.DrActive Then
+                If m_TopGUI.FX3.MeasurePinFreq(m_TopGUI.FX3.DrPin, 1, 100, 2) = Double.PositiveInfinity Then
+                    Dim res As DialogResult = MessageBox.Show("Warning, Data Ready not Toggling! Continue?", "Confirm Data Ready Sync", MessageBoxButtons.OKCancel)
+                    If res <> DialogResult.OK Then Exit Sub
+                End If
+            End If
+
+            'save file location
+            savePath = setSaveLocation(m_TopGUI.lastFilePath)
+            If savePath Is Nothing Then
+                MessageBox.Show("Please select a folder to save the stream data.", "Invalid save path!", MessageBoxButtons.OK)
+                Exit Sub
+            End If
+
+            'get numbursts
+            Try
+                numBursts = Convert.ToUInt32(numBurstWords.Text)
+            Catch ex As Exception
+                MsgBox("ERROR: Invalid number of bursts " + ex.Message)
+                Exit Sub
+            End Try
+
+            'get burst trigger
+            burstTrigger = UpdateBurstParams()
+            If IsNothing(burstTrigger) Then Exit Sub
+            m_TopGUI.FX3.BurstMOSIData = burstTrigger
+
+            'set up logger
+
+            For i As Integer = 0 To (m_numBytes >> 1) - 1
+                regs.Add(New RegClass With {.Address = 0, .Page = 0, .NumBytes = 2, .ReadLen = 16, .Label = "BURST_" + i.ToString()})
+            Next
+
+            If Not IsNothing(fileManager) Then
+                fileManager.Dispose()
+            End If
+
+            timeString = "_" + DateTime.Now().ToString("s")
+            timeString = timeString.Replace(":", "-")
+
+            fileManager = New Logger(m_TopGUI.FX3, m_TopGUI.Dut)
+
+            fileManager.RegList = regs
+            fileManager.FileBaseName = "BurstTest" + timeString
+            fileManager.FilePath = savePath
+            fileManager.Buffers = numBursts
+            fileManager.Captures = 1
+            fileManager.FileMaxDataRows = 1000000 'max you can open in excel
+            fileManager.BuffersPerWrite = 10000
+            fileManager.BufferTimeoutSeconds = 10 'Timeout in seconds
+
+            'hide other forms during stream
+            InteractWithOtherForms(True, Me)
+
+            'run async
+            fileManager.RunAsync()
+            btn_StartStream.Text = "Stop Burst Stream"
+            captureData.Enabled = False
+            applySettings.Enabled = False
+        Else
+            If fileManager.Busy Then
+                fileManager.CancelAsync()
+            Else
+                CleanupBurstStream()
+            End If
+        End If
+    End Sub
+
+    Private Sub BurstDoneHandler() Handles fileManager.RunAsyncCompleted
+        If Me.InvokeRequired Then
+            Me.BeginInvoke(Sub() CleanupBurstStream())
+        Else
+            CleanupBurstStream()
+        End If
+    End Sub
+
+    Private Sub CleanupBurstStream()
+        btn_StartStream.Text = "Start Burst Stream"
+        captureData.Enabled = True
+        applySettings.Enabled = True
+        'show other forms
+        InteractWithOtherForms(False, Me)
+    End Sub
+
+    Private Function UpdateBurstParams() As Byte()
+
+        Dim burstTrigger As New List(Of Byte)
+        Dim byteVal As Byte
+        Try
+            For i As Integer = 0 To m_numBytes - 1
+                byteVal = Convert.ToUInt32(result.Item("MOSI Value", i).Value, 16)
+                burstTrigger.Add(byteVal)
+            Next
+        Catch ex As Exception
+            MsgBox("ERROR: Invalid MOSI data. " + ex.Message)
+            Return Nothing
+        End Try
+        m_TopGUI.FX3.TriggerReg = New RegClass With {.Address = 0}
+        m_TopGUI.FX3.StripBurstTriggerWord = False
+        m_TopGUI.FX3.BurstByteCount = m_numBytes
+        m_TopGUI.FX3.SetupBurstMode()
+        Return burstTrigger.ToArray()
+
+    End Function
 
 End Class
