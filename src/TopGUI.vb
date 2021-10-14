6782cea8cf3f43ff371e3589011749446fdb22b3
Nolan <Alex.Nolan@analog.com>
2021-10-14 16:07:26 -0400

    Changed ADIS16210 default SCLK to 830KHz and stall to 40us to match the datasheet. Added X accel null register as a possible scratch register for testing SPI communications

--- src/TopGUI.vb
+++ src/TopGUI.vb
@@ -868,7 +868,7 @@ Public Class TopGUI
     Friend Function TestDUT() As Boolean
 
         Dim scratchReg As RegClass = Nothing
-        Dim scratchRegNames() As String = {"USER_SCRATCH", "USER_SCR1", "USER_SCR_2", "USER_SCR_1", "USER_SCRATCH_1", "ALM_MAG1", "APPLICATION_SPACE_0", "SCRATCH_A", "USER_ID"}
+        Dim scratchRegNames() As String = {"USER_SCRATCH", "USER_SCR1", "USER_SCR_2", "USER_SCR_1", "USER_SCRATCH_1", "ALM_MAG1", "APPLICATION_SPACE_3", "SCRATCH_A", "USER_ID", "XACCL_NULL"}
         Dim drActive As Boolean = FX3.DrActive
         Dim randomValue As UInteger
         Dim orignalScratch As UInteger
