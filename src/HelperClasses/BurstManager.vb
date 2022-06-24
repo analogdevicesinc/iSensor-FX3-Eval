377ad7117484608bc43347109c73edda71a1f267
Nolan <Alex.Nolan@analog.com>
2022-06-24 16:45:13 -0400

    Created a new class which will be used as a middle layer between the FX3 API and the Logger for pre-processing burst data prior to writing to file

---
+++ src/HelperClasses/BurstManager.vb
@@ -0,0 +1,39 @@
+﻿'Copyright (c) 2022 Analog Devices, Inc. All Rights Reserved.
+'This software is proprietary to Analog Devices, Inc. and its licensors.
+'
+'File:          BurstManager.vb
+'Author:        Alex Nolan (alex.nolan@analog.com)
+'Description:   Format burst packets based on the defined IMU burst protocol
+
+Imports System.ComponentModel
+Imports adisInterface
+Imports RegMapClasses
+Imports StreamDataLogger
+
+Public Class BurstManager
+    Implements IBufferedStreamProducer, IStreamEventProducer
+
+    Public Event NewBufferAvailable As IStreamEventProducer.NewBufferAvailableEventHandler Implements IStreamEventProducer.NewBufferAvailable
+    Public Event StreamFinished As IStreamEventProducer.StreamFinishedEventHandler Implements IStreamEventProducer.StreamFinished
+
+    Public Sub CancelStreamAsync() Implements IStreamEventProducer.CancelStreamAsync
+        Throw New NotImplementedException()
+    End Sub
+
+    Public Sub StartBufferedStream(regList As IEnumerable(Of RegClass), numCaptures As UInteger, numBuffers As UInteger, timeoutSeconds As Integer, worker As BackgroundWorker) Implements IBufferedStreamProducer.StartBufferedStream
+        Throw New NotImplementedException()
+    End Sub
+
+    Public Function ConvertReadDataToU32(regList As IEnumerable(Of RegClass), u16data As IEnumerable(Of UShort)) As UInteger() Implements IBufferedStreamProducer.ConvertReadDataToU32
+        Throw New NotImplementedException()
+    End Function
+
+    Public Function GetBufferedStreamDataPacket() As UShort() Implements IBufferedStreamProducer.GetBufferedStreamDataPacket
+        Throw New NotImplementedException()
+    End Function
+
+    Public Function ScaleRegData(regList As IEnumerable(Of RegClass), uintData As IEnumerable(Of UInteger)) As Double() Implements IBufferedStreamProducer.ScaleRegData
+        Throw New NotImplementedException()
+    End Function
+
+End Class
