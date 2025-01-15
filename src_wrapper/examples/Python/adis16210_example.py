c7395b061c1908e92c1c8c779a2d52c31bd7fe0c
Alex Nolan <Alex.Nolan@analog.com>
2025-01-15 11:12:47 -0500

    Updated to print scaled data

--- src_wrapper/examples/Python/adis16210_example.py
+++ src_wrapper/examples/Python/adis16210_example.py
@@ -35,17 +35,18 @@ print(Dut.FX3.GetFirmwareVersion)
 print("PROD_ID: " + str(Dut.ReadUnsigned("PROD_ID")))
 Dut.UserLEDBlink(2.0)
 
-#Create reg list
+#61ug per LSB
+accel_scale = 61.0 / 10**6
 
+#Create reg list
 regs_py = ['XACCL_OUT','YACCL_OUT','ZACCL_OUT','TEMP_OUT']
 regs = Array[String](regs_py)
 data = []
 
 while True:
     data = Dut.ReadSigned(regs)
-    for i in (data): 
-        print(i, end = " ")
-    print()
+    temp_c = (data[3] - 1331.0) * -0.47 #Zero C at 1331, -0.47C/LSB
+    print("XA: " + str(data[0] * accel_scale) + " YA: " + str(data[1] * accel_scale) + " ZA: " + str(data[2] * accel_scale) + " TEMP: " + str(temp_c))
     sleep(0.5)
 
 
